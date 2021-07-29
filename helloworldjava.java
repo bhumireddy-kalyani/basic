30a29b5dd14fcbaa1b0e24d056f7d87a4a8ec5de
bhumireddy-kalyani <88091728+bhumireddy-kalyani@users.noreply.github.com>
2021-07-29 14:42:08 +0530

    Create helloworldjava.java

---
+++ helloworldjava.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+© 2021 GitHub, Inc.
